b1ef9cada2bde1062a9885f8dc1f6466c7f4c302
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2024-02-07 17:36:01 +0100

    Added Sources

---
+++ src/main/java/it/cnr/iasi/saks/cc/benchmark/xacml/App.java
@@ -0,0 +1,13 @@
+package it.cnr.iasi.saks.cc.benchmark.xacml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
