497595d634a642f9b8f1b79d94d090d356cd0898
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2024-05-15 16:49:00 +0200

    Added Dummy test

---
+++ src/test/java/it/cnr/iasi/saks/cc/benchmark/xacml/tests/herasaf/DummyTest.java
@@ -0,0 +1,15 @@
+package it.cnr.iasi.saks.cc.benchmark.xacml.tests.herasaf;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+	
+	@Test
+	public void testConformance() throws Exception {
+		Assert.assertEquals(0, 0);
+	}
+
+	
+}
